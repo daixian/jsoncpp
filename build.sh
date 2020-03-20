3b1dfee511afd56314badb6856fb95934335e81a
daixian <amano_tooko@qq.com>
2020-03-21 01:01:49 +0800

    加了一个linux脚本给CI

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
-#! /bin/bash
+﻿#! /bin/bash
 
 echo "生成一个开发用的VS工程目录"
 
 export CC=/usr/bin/gcc-7
 export CXX=/usr/bin/g++-7
 
-conan create . daixian/stable -s compiler.version=15 -s compiler.runtime=MD -s arch=x86_64 -s build_type=Release --build missing
+export CONAN_REVISIONS_ENABLED=1
+
+conan create . daixian/stable -s compiler.version=7.5 -s arch=x86_64 -s build_type=Release --build missing
  
