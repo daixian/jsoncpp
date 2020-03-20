a8221511847aa67e93e1287741a936438a89ad80
daixian <amano_tooko@qq.com>
2020-03-20 13:03:09 +0800

    上传一下linux下的上传

---
+++ upload.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+conan upload boost --all -r=xuexue 
+conan upload bzip2 --all -r=xuexue 
+conan upload eigen --all -r=xuexue 
+conan upload gtest --all -r=xuexue 
+conan upload libjpeg --all -r=xuexue 
+conan upload libwebp --all -r=xuexue 
+conan upload opencv --all -r=xuexue 
+conan upload zlib --all -r=xuexue 
+conan upload openssl --all -r=xuexue 
+conan upload poco --all -r=xuexue 
+conan upload zlib --all -r=xuexue
